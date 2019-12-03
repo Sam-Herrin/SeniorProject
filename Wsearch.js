7a738a29b069d87d478ab300ebe28a25a0f64d19
Mark-Hodge <mrh598@msstate.edu>
2019-12-02 19:01:26 -0600

    Updated search field id

--- Wsearch.js
+++ Wsearch.js
@@ -1,4 +1,12 @@
 function search(){
     var val = document.getElementById("searchbox").value
     alert(val);
+	
+	var dbConnection = SQL.connect( { Driver: "MySQL",
+		Host: "localhost",
+		Port: 3306,
+	Database; "seniorProject" } );
+	
+	var sql = "SELECT CollectionTitle, SubjectHeadings, InclusiveDates, Institution
+		FROM theW
 }
