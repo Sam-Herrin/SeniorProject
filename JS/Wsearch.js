61c8cfd142958d08197043f39b31b1f03323419a
Connor Greenhill <connorgreenhill@hotmail.com>
2019-12-02 20:08:59 -0600

    Fixes to The W
    Slight fixes to the W Search

--- JS/Wsearch.js
+++ JS/Wsearch.js
@@ -1,5 +1,5 @@
 function search(){
-    var val = document.getElementById("searchbox").value;
+    var val = localStorage.getItem("searchVal");
     var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200){
@@ -8,4 +8,15 @@ function search(){
     };
     req.open("GET", "http://localhost/seniorproject/php/theWSearch.php?q=" + val, true);
     req.send();
+}
+
+function redir(){
+    localStorage.setItem("searchVal", document.getElementById("searchbox").value);
+    window.location = "http://localhost/seniorproject/Wresults.html";
+}
+
+window.onload = function(){
+
+    this.search();
+
 }
--- PHP/theWSearch.php
+++ PHP/theWSearch.php
@@ -13,9 +13,12 @@ if(!$result){
 if(mysqli_num_rows($result) != 0){
     while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
         echo "
-        <p>" . $row['CollectionTitle'] . "</p>
-        <p>" . $row['Institution'] . "</p>
-        <p>" . $row['InclusiveDates'] . "</p>";
+        <tr style='height: 100px'>
+            <td>" . $row['CollectionTitle'] . "</td>
+            <td>" . $row['Institution'] . "</td>
+            <td>" . $row['InclusiveDates'] . "</td>
+            <td>" . $row['SubjectHeadings'] . "</td>
+        </tr>";
     }
 }else{
     echo "No Results Matching Search";
